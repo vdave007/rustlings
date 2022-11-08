312ecfe1f2e7a76a60c6e1eb94fcd1c62ebd3737
Vincze David <vincdave@yahoo.com>
2022-11-08 12:57:58 +0000

    Completed primitive types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count: i64) -> i64 {
+    if count > 40 {
+        return count;
+    } else {
+        return count * 2;
+    }
+}
 
 // Don't modify this function!
 #[test]
